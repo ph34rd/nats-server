2bbf6c0209d9070a2e5a55f55baf7a809745c6db
ph34rd <ph34rd@gmail.com>
2024-12-12 15:46:27 +0300

    version bump to 2.10.23-fix

--- server/const.go
+++ server/const.go
@@ -55,7 +55,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.23"
+	VERSION = "2.10.23-fix"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
